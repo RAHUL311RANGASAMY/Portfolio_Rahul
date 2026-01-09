e25125a3835a23feedfec6d44625653fe01cdf78
Rahul <rahulrangasamy311@gmail.com>
2026-01-09 12:07:09 +0530

    add the cd to dev

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,32 @@
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// require('dotenv').config();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json());
+
+// // Routes
+// app.use('/api/contact', require('./routes/ContactRoutes'));
+
+// // MongoDB connect
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => console.log('MongoDB connected'))
+//   .catch((err) => console.log(err));
+
+// const PORT = 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
+
+
+
+
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
@@ -5,8 +34,13 @@ require('dotenv').config();
 
 const app = express();
 
-app.use(cors());
+// Middleware
 app.use(express.json());
+
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  methods: ["GET", "POST"],
+}));
 
 // Routes
 app.use('/api/contact', require('./routes/ContactRoutes'));
@@ -15,9 +49,11 @@ app.use('/api/contact', require('./routes/ContactRoutes'));
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
-  .catch((err) => console.log(err));
+  .catch((err) => console.error(err));
 
-const PORT = 5000;
+// Use Render's PORT
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- frontend/src/Components/Contact/Contact.jsx
+++ frontend/src/Components/Contact/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
     name: '',
     email: '',
     message: '',
-  });;
+  });
 
   //State for submission status
   const [status, setStatus] = useState('');
@@ -27,7 +27,12 @@ const Contact = () => {
     setStatus('Sending...');
 
     try {
-      const response = await fetch('http://localhost:5000/api/contact', {
+      // const response = await fetch('http://localhost:5000/api/contact', {
+      //   method: 'POST',
+      //   headers: { 'Content-Type': 'application/json' },
+      //   body: JSON.stringify(formData),
+      // });
+      const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/contact`, {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
@@ -43,7 +48,7 @@ const Contact = () => {
       }
     } catch (err) {
       console.error(err);
-      setStatus("Server error.Please try again later.");
+      setStatus('Server error.Please try again later.');
     }
   };
 
